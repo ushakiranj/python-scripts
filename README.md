83b295cacc343e860b663588654292362d3cb894
ushakiranj <65536784+ushakiranj@users.noreply.github.com>
2024-06-08 12:55:44 +0000

    First verified version

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # python-scripts
+
+This Python script formats the input paragraph text with both left and right justified per page width inputed.
+
+## Usage
+
+```shell
+
+# Example usage
+
+python3 String-manipulation.py <Paragraph text in string> <Page width in integer>
+
+```
+
+## Running the Tests
+
+You can run the tests using the following command:
+
+```shell
+
+python3 String-manipulation.py "This is a sample text but a complicated problem to be solved, so we are adding more text to see that it actually works." 20
+
+```
+
+![Script Execution](https://github.com/ushakiranj/python-scripts/blob/main/Script-Execution.gif)
--- String-manipulation.py
+++ String-manipulation.py
@@ -6,7 +6,7 @@ def splitParagraphPerPageWidth(paragraph, pagewidth):
     word        = ""
     listOfLines = []
 
-    listOfWords  = paragraph.split(' ')
+    listOfWords = paragraph.split(' ')
 
     for wordIdx in range(len(listOfWords)):
         word = (' ' if len(line) > 0 else '') + listOfWords[wordIdx]
@@ -17,8 +17,86 @@ def splitParagraphPerPageWidth(paragraph, pagewidth):
         else:
             listOfLines.append(line)
             line = listOfWords[wordIdx]
+            if wordIdx == len(listOfWords) - 1:
+                listOfLines.append(line)
+    return listOfLines
+
+def lrJustified(listOfParagraphLines, pagewidth):
+
+    listOfLines = []
+    listOfWords = []
+
+    for lineIdx in range(len(listOfParagraphLines)):
+
+        line                    = listOfParagraphLines[lineIdx]
+        newline                 = ""
+        totalSpacesToFill       = pagewidth - len(line)
+        totalWordsInALine       = 0
+        totalSpaceToFillBtwWord = 0
+
+        listOfWords             = line.split(' ')
+        totalWordsInALine       = (len(listOfWords) - 1)
+
+        # When there is only 1 word in a line, it is simply left justified
+        if totalWordsInALine == 0:
+            newline = line
+        # When atleast 1 space to be added between words    
+        elif (totalSpacesToFill // totalWordsInALine > 0):
+            
+            totalSpaceToFillBtwWord = totalSpacesToFill // totalWordsInALine
+
+            if totalSpacesToFill % totalWordsInALine > 0:
+                for wordIdx in range(len(listOfWords)):
+                    word = listOfWords[wordIdx]
+                    if wordIdx == len(listOfWords) - 1:
+                        newline = newline + (' ' if len(newline) > 0 else '') + word.rjust(len(word) + totalSpaceToFillBtwWord)
+                    else:
+                        newline = newline + (' ' if len(newline) > 0 else '') + word.ljust(len(word) + totalSpaceToFillBtwWord)    
+            else:
+                for wordIdx in range(len(listOfWords)):
+                    word = listOfWords[wordIdx]
+                    if wordIdx == len(listOfWords) - 1:
+                        newline = newline + (' ' if len(newline) > 0 else '') + word.rjust(len(word))
+                    else:    
+                        newline = newline + (' ' if len(newline) > 0 else '') + word.ljust(len(word) + totalSpaceToFillBtwWord)
+        else:
+            for wordIdx in range(len(listOfWords)):
+                word = listOfWords[wordIdx]
+                if wordIdx == len(listOfWords) - 1:
+                    newline = newline + (' ' if len(newline) > 0 else '') + word.rjust(len(word) + totalSpacesToFill)
+                else:    
+                    newline = newline + (' ' if len(newline) > 0 else '') + word.ljust(len(word))
+
+        listOfLines.append(newline)
 
     return listOfLines
 
-formatedParagraph = splitParagraphPerPageWidth(sys.argv[1], int(sys.argv[2]))
-print(formatedParagraph)
+def validateInputArguments():
+    errMsg            = f"Usage: python {sys.argv[0]} 'This is a sample paragraph text that need to be left and right justified by the script as per page width provided.' 20"
+    expectedArguments = 2
+
+    if len(sys.argv) != expectedArguments + 1:
+        print(f"Error: Expected {expectedArguments} parameters!")
+        print(errMsg)
+        sys.exit(1)
+    else:
+        paragraph = sys.argv[1]
+        pagewidth = sys.argv[2]
+
+        if not paragraph or not pagewidth:
+            print(f"Error: Paragraph text or page width is empty!")
+            print(errMsg)
+            sys.exit(1)
+        elif not pagewidth.isnumeric():
+            print(f"Error: Page width can only be numeric integer!")
+            print(errMsg)
+            sys.exit(1)
+    return None
+
+validateInputArguments()
+splittedParagraph = splitParagraphPerPageWidth(sys.argv[1], int(sys.argv[2]))
+if len(splittedParagraph) > 0:
+    formatedParagraph = lrJustified(splittedParagraph, int(sys.argv[2]))
+    print()
+    for lineIdx in range(len(formatedParagraph)):
+        print(formatedParagraph[lineIdx])
